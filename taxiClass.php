1f8fcc2ed43c1a1347e57ad9ef55c53454dc21f1
ei17ringo <ei17ringo@gmail.com>
2016-01-04 20:24:24 +0800

    タクシーが動くアニメーション入り

---
+++ taxiClass.php
@@ -0,0 +1,18 @@
+<?php
+
+class Car{
+		
+	function engine(){
+		header("Location:./taxi/engine.html");
+	}
+
+	function run(){
+		header("Location:./taxi/run.html");
+	}
+
+	function stop(){
+		header("Location:./taxi/stop.html");
+	}
+}
+
+?>
---
+++ taxi_demo.php
@@ -0,0 +1,43 @@
+<?php
+
+	require('taxiClass.php');
+
+	$yellow_taxi = new Car();
+
+	if (isset($_REQUEST['action'])){
+		$action = $_REQUEST['action'];
+		switch ($action) {
+			case 'engine':
+				$yellow_taxi->engine();
+				break;
+			case 'run':
+				$yellow_taxi->run();
+				break;
+			case 'stop':
+				$yellow_taxi->stop();
+				break;
+			default:
+				# code...
+				break;
+		}
+
+
+	}
+
+
+?>
+
+
+<html>
+<body>
+	<form method="post" action="taxi_demo.php?action=engine">
+		<input type="submit" value="エンジンを掛ける" />
+	</form>
+	<form method="post" action="taxi_demo.php?action=run">
+		<input type="submit" value="走る" />
+	</form>
+	<form method="post" action="taxi_demo.php?action=stop">
+		<input type="submit" value="止める" />
+	</form>
+</body>
+</html>
